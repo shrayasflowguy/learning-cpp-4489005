b753383f569aab1e83f5c22a6ba25b058b23142e
shrayasflowguy <shrayaspavithran@gmail.com>
2025-04-15 18:13:32 +0000

    Modification

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Learning C++ 
-// Exercise 00_03
+// Exercise 00_03 with modification 
 // Using the exercise files on GitHub, by Eduardo Corpeño 
 
 #include <iostream>
@@ -9,12 +9,13 @@ int main(){
 
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
+    std::cout << std::endl << std::endl;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
     
     result = num_1 + num_2;
     
-    std::cout << "The result of the addition is " << result << std::endl;
+    std::cout << "The result of the addition is " << result << "result +1 " << result +1 << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
