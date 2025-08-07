1044912a96901b947846a8c1cb350158e510291b
shrayasflowguy <shrayaspavithran@gmail.com>
2025-08-07 13:03:28 +0000

    Added integers

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,17 @@
 
 #include <iostream>
 
+int a, b=10; // Integers 
+/*
+Multi-line comments
+*/
 int main(){
+    bool yes_or_no;
     std::cout << "Hi There!" << std::endl;
-    
+    a=5;
+    std::cout<<"a+b="<< a+b<< std::endl;
+    yes_or_no=false;
+    std::cout<<yes_or_no<<std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
