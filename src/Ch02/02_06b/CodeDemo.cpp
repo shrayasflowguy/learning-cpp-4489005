0ac3710643c6cabedc9c0a97c97ebe6c127607ad
shrayasflowguy <shrayaspavithran@gmail.com>
2025-08-07 13:48:13 +0000

    If def added

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,20 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
+#define CAPACITY 5000
+#define DEBUG
 
 int main(){
-    
+    int32_t large = CAPACITY;
+    int8_t small= 8;
+#ifdef DEBUG
+    std::cout << "debugging" << std::endl;
+#endif
+    large += small;
+    std::cout<< "large = " << large << std::endl;
     std::cout << std::endl << std::endl;
+
     return (0);
 }
